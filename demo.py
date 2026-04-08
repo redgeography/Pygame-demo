516d5f0e325db43ca3b0091b539d3e7b6b2d8c27
redgeography <128873224+redgeography@users.noreply.github.com>
2026-04-08 16:43:51 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
 import pygame
-from threading import thread
+from threading import Thread
 from time import sleep
 from pyautogui import size
 
 pygame.init()
-class Speed:
-  def __init__(speed, accel, accel_rate, terminal_velocity, decel_rate = accel_rate * 2):
+
     
